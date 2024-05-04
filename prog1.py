56903801cd21adbe920f35851233ab9e594f32af
Isabel <schoolaccount@MacBook-Pro-5.local>
2024-05-04 06:39:04 -0400

    created first program in python, fibonacci sequence using recursion

---
+++ prog1.py
@@ -0,0 +1,14 @@
+'''write a function fib(n) that takes in a number
+as an argument. The function should return the n-th number
+of the Fibonacci sequence.
+
+The first and second number of the sequence is 1.
+To generate the next number of the sequence, we sum the previous two.'''
+
+def fib(n):
+    if (n<=2): return 1
+    return fib(n-1) + fib(n-2)
+
+print(fib(6))
+print(fib(7))
+print(fib(8))
